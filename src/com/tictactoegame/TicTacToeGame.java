fa968f056e9d65771640e2b4e373c815c463f9b9
Kajalwankhede <kajuwankhede1510@gmail.com>
2022-11-21 12:26:41 +0530

    UC2- Ability to choose letter

--- src/com/tictactoegame/TicTacToeGame.java
+++ src/com/tictactoegame/TicTacToeGame.java
@@ -1,11 +1,15 @@
 package com.tictactoegame;
-
+import java.util.Scanner;
 public class TicTacToeGame {
+    static Scanner sc=new Scanner(System.in);
+    private static char computer;
+    private static char player;
+
     public static void main(String[] args) {
     char[][] gameBoard = {{'_', '|', '_', '|', '_'}, {'_', '|', '_', '|', '_'}, {' ', '|', ' ', '|', ' '}};
     dispayBoard(gameBoard);
+    chooseLetter();
 }
-
     public static void dispayBoard(char[][] gameBoard) {  ///Display the game board
         System.out.println("\n*** WELCOME TO TIC TAC TOE GAME ***");
         for (int row = 0; row < gameBoard.length; row++) {  /// for the rows
@@ -15,4 +19,14 @@ public class TicTacToeGame {
             System.out.println();
         }
     }
-}
+    public static void chooseLetter(){
+        System.out.println("Choose Letter X or O");
+        char letter=sc.next().charAt(0);
+        if (letter=='X' || letter =='x'){
+           player='X';
+        }
+        else  if (letter=='O' || letter =='o'){
+                player='O';
+            }System.out.println("Player Choose letter:" +player);
+    }
+    }
