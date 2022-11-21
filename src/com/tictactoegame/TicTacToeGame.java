df327e6b13ad978629a4c5c2d1eb8c3dfcd8aef5
Kajalwankhede <kajuwankhede1510@gmail.com>
2022-11-21 12:01:52 +0530

    UC1 - Creating a tic tac toe game board

--- src/com/tictactoegame/TicTacToeGame.java
+++ src/com/tictactoegame/TicTacToeGame.java
@@ -2,6 +2,17 @@ package com.tictactoegame;
 
 public class TicTacToeGame {
     public static void main(String[] args) {
-        System.out.println("**************** Welcome TO TicTacToe Game *********************");
+    char[][] gameBoard = {{'_', '|', '_', '|', '_'}, {'_', '|', '_', '|', '_'}, {' ', '|', ' ', '|', ' '}};
+    dispayBoard(gameBoard);
+}
+
+    public static void dispayBoard(char[][] gameBoard) {  ///Display the game board
+        System.out.println("\n*** WELCOME TO TIC TAC TOE GAME ***");
+        for (int row = 0; row < gameBoard.length; row++) {  /// for the rows
+            for (int col = 0; col < gameBoard[0].length; col++) {// for column
+                System.out.print(gameBoard[row][col]);
+            }
+            System.out.println();
+        }
     }
 }
